c6565aec1a1c8bd40174770b3ce728c94e0d7af0
deririzz <183678853+deririzkyip@users.noreply.github.com>
2026-07-20 13:32:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,47 @@
+// CONFIGURATION UNDANGAN DIGITAL
+// Anda bisa mengubah isi di bawah ini dengan mudah tanpa perlu mengubah file HTML/JS lainnya.
+
+const weddingConfig = {
+    // 1. NAMA MEMPELAI
+    mempelaiPria: "Romeo Montague",
+    mempelaiPriaShort: "Romeo",
+    mempelaiPriaOrtu: "Bpk. Montague & Ibu Lady Montague",
+    mempelaiPriaInstagram: "#", // ganti dengan link instagram jika ada
+
+    mempelaiWanita: "Juliet Capulet",
+    mempelaiWanitaShort: "Juliet",
+    mempelaiWanitaOrtu: "Bpk. Capulet & Ibu Lady Capulet",
+    mempelaiWanitaInstagram: "#", // ganti dengan link instagram jika ada
+
+    // 2. TANGGAL & COUNTDOWN
+    // Format countdown harus bahasa Inggris: "Month Day, Year HH:MM:SS" (Contoh: "October 12, 2026 08:00:00")
+    countdownDate: "October 12, 2026 08:00:00",
+
+    // Tampilan Tanggal di Cover & Hero
+    weddingDateSimple: "Minggu, 12 Oktober 2026",
+    weddingDateDots: "12 . 10 . 2026", // format titik di banner hero
+
+    // 3. DETAIL ACARA
+    // Akad Nikah
+    akadDate: "Minggu, 12 Oktober 2026",
+    akadTime: "08.00 WIB - 10.00 WIB",
+    akadLocationName: "Masjid Raya Al-Akbar",
+    akadLocationAddress: "Jl. Masjid Al-Akbar Timur No.1, Surabaya",
+
+    // Resepsi Pernikahan
+    resepsiDate: "Minggu, 12 Oktober 2026",
+    resepsiTime: "11.00 WIB - 14.00 WIB",
+    resepsiLocationName: "Gedung Grand Ballroom Al-Akbar",
+    resepsiLocationAddress: "Jl. Masjid Al-Akbar Timur No.1, Surabaya",
+
+    // 4. LOKASI GOOGLE MAPS
+    // Copy link Google Maps iframe (cari di google maps -> bagikan -> sematkan peta -> copy link src saja)
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.280807759881!2d112.71261777590822!3d-7.322332672023162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb764d2cf99b%3A0x633519808d4b3b28!2sMasjid%20Nasional%20Al-Akbar%20Surabaya!5e0!3m2!1sid!2sid!4v1700000000000!5m2!1sid!2sid",
+
+    // Link tombol Google Maps (untuk dibuka di aplikasi HP)
+    mapsButtonUrl: "https://maps.app.goo.gl/kvhMSTcEKkxMEYCP7?g_st=ic",
+
+    // 5. TOMBOL SIMPAN TANGGAL (GOOGLE CALENDAR)
+    // Buat link kalender di https://www.calendar.google.com jika ingin menyesuaikan
+    calendarUrl: "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Romeo+%26+Juliet&dates=20261012T010000Z/20261012T070000Z&details=Pernikahan+Romeo+Juliet&location=Masjid+Nasional+Al-Akbar+Surabaya"
+};
