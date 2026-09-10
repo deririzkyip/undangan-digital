f6eee8e43ed4e95ad158bfc618dec91deec90794
deririzz <183678853+deririzkyip@users.noreply.github.com>
2026-09-10 22:35:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,43 +5,43 @@ const weddingConfig = {
     // 1. NAMA MEMPELAI
     mempelaiPria: "Aldi",
     mempelaiPriaShort: "Aldi",
-    mempelaiPriaOrtu: "Bpk. Montague & Ibu Lady Montague",
-    mempelaiPriaInstagram: "#", // ganti dengan link instagram jika ada
+    mempelaiPriaOrtu: "Bpk. Alm. Soleman & Ibu. Sukni",
+    mempelaiPriaInstagram: "#",
 
-    mempelaiWanita: "Rika",
-    mempelaiWanitaShort: "Rika",
-    mempelaiWanitaOrtu: "Bpk. Capulet & Ibu Lady Capulet",
-    mempelaiWanitaInstagram: "#", // ganti dengan link instagram jika ada
+    mempelaiWanita: "Rika Julianti",
+    mempelaiWanitaShort: "Rika Julianti",
+    mempelaiWanitaOrtu: "Bpk. Suginah & Ibu. Eti",
+    mempelaiWanitaInstagram: "#",
 
     // 2. TANGGAL & COUNTDOWN
-    // Format countdown harus bahasa Inggris: "Month Day, Year HH:MM:SS" (Contoh: "October 12, 2026 08:00:00")
-    countdownDate: "October 12, 2026 08:00:00",
+    // Format countdown harus bahasa Inggris: "Month Day, Year HH:MM:SS" (Contoh: "September 27, 2026 08:00:00")
+    countdownDate: "September 27, 2026 09:00:00",
 
     // Tampilan Tanggal di Cover & Hero
-    weddingDateSimple: "Minggu, 12 Oktober 2026",
-    weddingDateDots: "12 . 10 . 2026", // format titik di banner hero
+    weddingDateSimple: "Minggu, 27 September 2026",
+    weddingDateDots: "27 . 09 . 2026",
 
     // 3. DETAIL ACARA
     // Akad Nikah
-    akadDate: "Minggu, 12 Oktober 2026",
-    akadTime: "08.00 WIB - 10.00 WIB",
-    akadLocationName: "Masjid Raya Al-Akbar",
-    akadLocationAddress: "Jl. Masjid Al-Akbar Timur No.1, Surabaya",
+    akadDate: "Minggu, 27 September 2026",
+    akadTime: "09.00 WIB - 10.00 WIB",
+    akadLocationName: "Kediaman Mempelai Wanita",
+    akadLocationAddress: "kp.ciledok RT 05 RW 06 Des. mekarsri Kec. Panimbang",
 
     // Resepsi Pernikahan
-    resepsiDate: "Minggu, 12 Oktober 2026",
-    resepsiTime: "11.00 WIB - 14.00 WIB",
-    resepsiLocationName: "Gedung Grand Ballroom Al-Akbar",
-    resepsiLocationAddress: "Jl. Masjid Al-Akbar Timur No.1, Surabaya",
+    resepsiDate: "Minggu, 27 September 2026",
+    resepsiTime: "11.00 WIB - Selesai",
+    resepsiLocationName: "Kediaman Mempelai Wanita",
+    resepsiLocationAddress: "kp.ciledok RT 05 RW 06 Des. mekarsri Kec. Panimbang",
 
     // 4. LOKASI GOOGLE MAPS
     // Copy link Google Maps iframe (cari di google maps -> bagikan -> sematkan peta -> copy link src saja)
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.280807759881!2d112.71261777590822!3d-7.322332672023162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb764d2cf99b%3A0x633519808d4b3b28!2sMasjid%20Nasional%20Al-Akbar%20Surabaya!5e0!3m2!1sid!2sid!4v1700000000000!5m2!1sid!2sid",
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d416.6528464549787!2d105.74750493190689!3d-6.535291880869842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e43b5de1a7a4dd5%3A0x35bb1e2cbb846c78!2sToko%20FatihFatar!5e0!3m2!1sen!2sid!4v1789051349542!5m2!1sen!2sid",
 
     // Link tombol Google Maps (untuk dibuka di aplikasi HP)
-    mapsButtonUrl: "https://maps.app.goo.gl/GEKcXHsmS1pLHpNs9",
+    mapsButtonUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d416.6528464549787!2d105.74750493190689!3d-6.535291880869842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e43b5de1a7a4dd5%3A0x35bb1e2cbb846c78!2sToko%20FatihFatar!5e0!3m2!1sen!2sid!4v1789051349542!5m2!1sen!2sid",
 
     // 5. TOMBOL SIMPAN TANGGAL (GOOGLE CALENDAR)
     // Buat link kalender di https://www.calendar.google.com jika ingin menyesuaikan
-    calendarUrl: "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Romeo+%26+Juliet&dates=20261012T010000Z/20261012T070000Z&details=Pernikahan+Romeo+Juliet&location=Masjid+Nasional+Al-Akbar+Surabaya"
+    calendarUrl: "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Aldi+%26+Rika+Julianti&dates=20260927T000000Z/20260927T230000Z&details=Pernikahan+Aldi+%26+Rika+Julianti&location=Toko+FatihFatar"
 };
