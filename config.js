7faf6a7b7ebcf19bb01a5f426e0560dfb14c7e8f
deririzz <183678853+deririzkyip@users.noreply.github.com>
2026-09-11 00:28:29 +0700

    Add Google Script URL for confirmation and prayer

--- config.js
+++ config.js
@@ -41,7 +41,10 @@ const weddingConfig = {
     // Link tombol Google Maps (untuk dibuka di aplikasi HP)
     mapsButtonUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d416.6528464549787!2d105.74750493190689!3d-6.535291880869842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e43b5de1a7a4dd5%3A0x35bb1e2cbb846c78!2sToko%20FatihFatar!5e0!3m2!1sen!2sid!4v1789051349542!5m2!1sen!2sid",
 
-    // 5. TOMBOL SIMPAN TANGGAL (GOOGLE CALENDAR)
+    // 5. GOOGLE SCRIPT URL (untuk Konfirmasi & Doa Restu)
+    googleScriptUrl: "https://script.google.com/macros/s/AKfycbwt28m4VU419_DCW1INTfeC13ajifBCyrnwS0fb01UN_gCHLhFN51ja28UJzjV-Tb9rKg/exec",
+
+    // 6. TOMBOL SIMPAN TANGGAL (GOOGLE CALENDAR)
     // Buat link kalender di https://www.calendar.google.com jika ingin menyesuaikan
     calendarUrl: "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Aldi+%26+Rika+Julianti&dates=20260927T000000Z/20260927T230000Z&details=Pernikahan+Aldi+%26+Rika+Julianti&location=Toko+FatihFatar"
 };
